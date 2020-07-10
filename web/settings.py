75cf42511077b6d1c06f0933c0b6342b74f49f03
Nichita Vatamaniuc <38537228+Neqit@users.noreply.github.com>
2020-07-10 22:07:41 +0200

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'niqvat@gmail.com'
-EMAIL_HOST_PASSWORD = 'Qwerty123!'
+EMAIL_HOST_USER = 'your@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_gmail_password'
